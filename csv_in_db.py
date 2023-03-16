6316b558edfb91748c6881419dedd558dd64ee1b
WORK_user <www.pavel_ru.ru@mail.ru>
2023-03-16 17:42:39 +0300

    Первая версия добавления данных в фрейм pandas и в базу данных postgresql

---
+++ csv_in_db.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+# Establish a connection
+password = 'root'
+engine = create_engine(f'postgresql://test:{password}@localhost:5432/test')
+# conn = psycopg2.connect(database="postgres", user="postgres", password="root123!@#", host="localhost", port="5432")
+# Create a new cursor
+# Read the excel file
+df = pd.read_csv('cashe.csv', encoding="utf-8")
+print(df.info())
+df.to_sql(name='table_test', con=engine, if_exists='replace', index=False)
+
