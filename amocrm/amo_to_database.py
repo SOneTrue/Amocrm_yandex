72bf7a21dc80f4492e8e9eda220885ce5f4f4c36
WORK_user <www.pavel_ru.ru@mail.ru>
2023-03-21 18:33:18 +0300

    Сделан список ненужных колонок + их дроп в DataFrame pandas

--- amocrm/amo_to_database.py
+++ amocrm/amo_to_database.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+from amocrm.column_drop import column
+
 # Establish a connection
 password = 'root'
 engine = create_engine(f'postgresql://test:{password}@localhost:5432/test')
 
 # Read the excel file
-df = pd.read_csv(r'amocrm_export_leads.csv', encoding="utf-8")
-print(df.info())
+df = pd.read_csv(r'./amocrm_export_leads.csv', encoding="utf-8")
+df = df.drop(column, axis=1)
+
 try:
     df.to_sql(name='excel', con=engine, if_exists='append', index=False)
 except:
